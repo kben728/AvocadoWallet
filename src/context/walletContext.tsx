3d42bbb497a62ab78b047a1dcd59cce1750095e6
kben728 <kim.benjamin.728@gmail.com>
2023-12-16 16:07:13 -0500

    Fix: Minor Changes And Fixes

--- src/context/walletContext.tsx
+++ src/context/walletContext.tsx
@@ -5,25 +5,27 @@ import {sendAllTokens} from '../utils/transactions.util';
 import {
   createAvocadoWallet,
   fetchAccountErc20Balances,
-  fetchAvoBalances,
-  generateNewWallet
+  generateNewWallet,
+  recoverWalletFromPrivateKey
 } from '../utils/wallet.util';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+import {Wallet} from 'ethers';
 
 interface TokenBalances {
   [tokenName: string]: string;
 }
-interface AvocadoBalances {
-  polygon: string;
-}
 
 interface WalletContextProps {
-  wallet: HDNodeWallet | undefined;
+  wallet: HDNodeWallet | undefined | Wallet;
   tokenBalances: TokenBalances;
-  avocadoBalance: AvocadoBalances;
+  avocadoBalance: TokenBalances;
   handleWalletGeneration: () => Promise<void>;
   handleFundsTransferToAvocado: () => Promise<void>;
+  handleWalletRecoveryFromPrivateKey: (key: string) => Promise<void>;
   avocadoWallet: string;
   loading: boolean;
+  recoveryLoading: boolean;
   creationLoading: boolean;
 }
 
@@ -33,7 +35,13 @@ const DEFAULT_TOKEN_BALANCES: TokenBalances = {
   'optimism-USDT': '0',
   'arbitrum-USDT': '0'
 };
-const DEFAULT_AVOCADO_BALANCE: AvocadoBalances = {polygon: '0'};
+const DEFAULT_AVOCADO_BALANCE: TokenBalances = {
+  'polygon-USDC': '0',
+  'ethereum-USDT': '0',
+  'optimism-USDT': '0',
+  'arbitrum-USDT': '0'
+};
+const WALLET_STORAGE_KEY = 'wallet';
 
 const WalletContext = createContext<WalletContextProps>({
   wallet: undefined,
@@ -42,17 +50,61 @@ const WalletContext = createContext<WalletContextProps>({
   handleWalletGeneration: async () => {},
   avocadoWallet: '',
   handleFundsTransferToAvocado: async () => {},
+  handleWalletRecoveryFromPrivateKey: async () => {},
   loading: false,
-  creationLoading: false
+  creationLoading: false,
+  recoveryLoading: false
 });
 
 const WalletProvider: React.FC<{children: React.ReactNode}> = ({children}) => {
-  const [wallet, setWallet] = useState<HDNodeWallet | undefined>();
+  const [wallet, setWallet] = useState<HDNodeWallet | undefined | Wallet>();
   const [avocadoWallet, setAvocadoWallet] = useState<string>();
   const [loading, setLoading] = useState(false);
   const [creationLoading, setCreationLoading] = useState(false);
+  const [recoveryLoading, setRecoveryLoading] = useState(false);
   const [tokenBalances, setTokenBalances] = useState(DEFAULT_TOKEN_BALANCES);
   const [avocadoBalance, setAvocadoBalance] = useState(DEFAULT_AVOCADO_BALANCE);
+
+  const refreshAvocadoAndBalances = async (wall: Wallet | HDNodeWallet) => {
+    const aWallet = await createAvocadoWallet(wall);
+    setAvocadoWallet(aWallet);
+
+    const balances = await fetchAccountErc20Balances(wall.address);
+    setTokenBalances(balances);
+
+    const avocadoBalances = await fetchAccountErc20Balances(aWallet);
+    setAvocadoBalance(avocadoBalances);
+  };
+
+  const loadWalletFromStorage = async () => {
+    try {
+      const walletData = await AsyncStorage.getItem(WALLET_STORAGE_KEY);
+
+      if (walletData) {
+        const loadedWallet = JSON.parse(walletData);
+
+        setWallet(loadedWallet);
+        refreshAvocadoAndBalances(loadedWallet);
+        Toast.show({
+          type: 'success',
+          text1: 'Wallet Loaded',
+          text2: 'Your wallet has been loaded from local storage'
+        });
+      } else {
+        handleWalletGeneration();
+      }
+    } catch (error) {
+      console.error('Error loading wallet from storage:', error);
+    }
+  };
+
+  const saveWalletToStorage = async (newWallet: any) => {
+    try {
+      await AsyncStorage.setItem(WALLET_STORAGE_KEY, JSON.stringify(newWallet));
+    } catch (error) {
+      console.error('Error saving wallet to storage:', error);
+    }
+  };
 
   const handleWalletGeneration = async () => {
     setCreationLoading(true);
@@ -61,15 +113,13 @@ const WalletProvider: React.FC<{children: React.ReactNode}> = ({children}) => {
       try {
         const newWallet = generateNewWallet();
         setWallet(newWallet);
-
-        handleAccountBalances();
-        const aWallet = await createAvocadoWallet(newWallet);
-        setAvocadoWallet(aWallet);
-
-        const avocadoBalances = await fetchAvoBalances(newWallet.address);
-        setAvocadoBalance({
-          polygon: avocadoBalances ?? DEFAULT_AVOCADO_BALANCE.polygon
+        await saveWalletToStorage({
+          ...newWallet,
+          privateKey: newWallet.privateKey
         });
+
+        refreshAvocadoAndBalances(newWallet);
+
         Toast.show({
           type: 'success',
           text1: 'Wallet Created',
@@ -89,16 +139,47 @@ const WalletProvider: React.FC<{children: React.ReactNode}> = ({children}) => {
     }, 500);
   };
 
+  const handleWalletRecoveryFromPrivateKey = async (privateKey: string) => {
+    setRecoveryLoading(true);
+
+    try {
+      const newWallet = await recoverWalletFromPrivateKey(privateKey);
+      setWallet(newWallet);
+      saveWalletToStorage({...newWallet, privateKey: newWallet.privateKey});
+
+      await refreshAvocadoAndBalances(newWallet);
+
+      Toast.show({
+        type: 'success',
+        text1: 'Wallet Recovered',
+        text2: 'Your wallet has been created successfully'
+      });
+    } catch (error) {
+      console.log('Error during wallet generation:', error);
+
+      Toast.show({
+        type: 'error',
+        text1: 'Wallet Recover Failed',
+        text2: 'Please check your private key and try again'
+      });
+    } finally {
+      setRecoveryLoading(false);
+    }
+  };
   const handleAccountBalances = async () => {
     if (!wallet) return;
 
     const balances = await fetchAccountErc20Balances(wallet.address);
     setTokenBalances(balances);
 
-    const avocadoBalances = await fetchAvoBalances(wallet.address);
-    setAvocadoBalance({
-      polygon: avocadoBalances ?? DEFAULT_AVOCADO_BALANCE.polygon
-    });
+    if (!avocadoWallet) return;
+    const avocadoBalances = await fetchAccountErc20Balances(avocadoWallet);
+    setAvocadoBalance(avocadoBalances);
+
+    // Toast.show({
+    //   type: 'success',
+    //   text1: 'Balance Updated'
+    // });
   };
 
   const handleFundsTransferToAvocado = async () => {
@@ -107,6 +188,7 @@ const WalletProvider: React.FC<{children: React.ReactNode}> = ({children}) => {
 
       if (!wallet || !avocadoWallet) return;
       await sendAllTokens(wallet.privateKey, avocadoWallet);
+      handleAccountBalances();
     } catch (err) {
       // Handle the error if needed
     } finally {
@@ -115,9 +197,8 @@ const WalletProvider: React.FC<{children: React.ReactNode}> = ({children}) => {
   };
 
   useEffect(() => {
-    if (!wallet) handleWalletGeneration();
+    loadWalletFromStorage();
   }, []);
-
   const value: WalletContextProps = {
     handleWalletGeneration,
     wallet,
@@ -126,7 +207,9 @@ const WalletProvider: React.FC<{children: React.ReactNode}> = ({children}) => {
     handleFundsTransferToAvocado,
     loading,
     creationLoading,
-    avocadoBalance
+    avocadoBalance,
+    handleWalletRecoveryFromPrivateKey,
+    recoveryLoading
   };
 
   return (
